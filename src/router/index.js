83b0de8c3b8b70432b927f85b30b3ba9124cccdb
xwakeup <619678987@qq.com>
2018-11-08 14:41:37 +0800

    项目初始化

---
+++ src/router/index.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Login from '@/components/Login.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {path: '/login', component: Login},
+    {path: '/', redirect: '/login'}
+  ]
+})
